1a02b90e169fac471342da1a38df55d69e3df4f3
ALI MUSA ALHASHIM <93006102+ali-alhashim@users.noreply.github.com>
2023-04-21 03:08:27 +0300

    start writing django code

--- alsuwq/settings.py
+++ alsuwq/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,11 +38,23 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    ## my Apps
+    'django.contrib.humanize',
+    'user',
+    'cart',
+    'category',
+    'order',
+    'payment',
+    'product',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    ## for lang
+    'django.middleware.locale.LocaleMiddleware',
+    
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -54,7 +67,7 @@ ROOT_URLCONF = 'alsuwq.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -62,6 +75,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
+                ## for lang.
+                'django.template.context_processors.i18n',
             ],
         },
     },
@@ -69,6 +85,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'alsuwq.wsgi.application'
 
+AUTH_USER_MODEL  = 'user.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
@@ -105,11 +122,20 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Riyadh'
 
 USE_I18N = True
 
 USE_TZ = True
+
+
+LANGUAGES = [
+                ("ar", ("Arabic")),
+                ("en", ("English")),
+    
+            ]
+
+LOCALE_PATHS = (str(BASE_DIR / "locale"),)
 
 
 # Static files (CSS, JavaScript, Images)
@@ -117,7 +143,36 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+if DEBUG:
+    STATICFILES_DIRS = [
+                         BASE_DIR / 'static',
+                        ]
+
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+   
+}
+
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ali.0560497943@gmail.com'
+EMAIL_HOST_PASSWORD = 'ciddzjkfzqxabhzw'
+EMAIL_TIMEOUT = 100
--- alsuwq/urls.py
+++ alsuwq/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('i18n/', include('django.conf.urls.i18n')),
 ]
+
+urlpatterns  += i18n_patterns (
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+
+)+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
